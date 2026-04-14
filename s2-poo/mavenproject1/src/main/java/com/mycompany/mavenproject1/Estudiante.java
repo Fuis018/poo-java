b21cfe2d4d71465180d2d2232060551cadf85f1a
Fuis018 <fuis18larc01z@gmail.com>
2026-04-14 16:07:30 -0500

    Add files via upload

--- s2-poo/mavenproject1/src/main/java/com/mycompany/mavenproject1/Estudiante.java
+++ s2-poo/mavenproject1/src/main/java/com/mycompany/mavenproject1/Estudiante.java
@@ -1,38 +1,56 @@
 
-/**
- * @author UCV-PIURA
- * @version 1.0
- * @created 14-Abr.-2026 14:13:06
- */
 public class Estudiante {
 
-	private string Apellido;
-	private string Edad;
-	private string Nombre;
+    private String apellido;
+    private int edad;
+    private String nombre;
 
-	public Estudiante Yo() {
+    // Constructor
+    public Estudiante(String nombre, String apellido, int edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+    }
 
-	}
+    // Método sumar
+    public int sumar(int num1, int num2) {
+        return num1 + num2;
+    }
 
-	public void finalize() throws Throwable {
+    // Método restar
+    public int restar(int num2, int num3) {
+        return num2 - num3;
+    }
 
-	}
+    // Mostrar datos del estudiante
+    public void mostrarDatos() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Apellido: " + apellido);
+        System.out.println("Edad: " + edad);
+    }
 
-	/**
-	 * 
-	 * @param num2
-	 * @param num3
-	 */
-	public void restar(int num2, int num3) {
+    // Getters y Setters
+    public String getNombre() {
+        return nombre;
+    }
 
-	}
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
-	/**
-	 * 
-	 * @param num1
-	 * @param num2
-	 */
-	public void sumar(int num1, int num2) {
+    public String getApellido() {
+        return apellido;
+    }
 
-	}
-}// end Estudiante Yo
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+}
