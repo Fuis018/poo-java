985cd2401ba9b9b5c07dc93fee3b3f4f4e0e2e41
Fuis018 <fuis18larc01z@gmail.com>
2026-04-14 14:21:57 -0500

    Add files via upload

---
+++ s2-poo/mavenproject1/src/main/java/com/mycompany/mavenproject1/Estudiante.java
@@ -0,0 +1,38 @@
+
+/**
+ * @author UCV-PIURA
+ * @version 1.0
+ * @created 14-Abr.-2026 14:13:06
+ */
+public class Estudiante {
+
+	private string Apellido;
+	private string Edad;
+	private string Nombre;
+
+	public Estudiante Yo() {
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+
+	/**
+	 * 
+	 * @param num2
+	 * @param num3
+	 */
+	public void restar(int num2, int num3) {
+
+	}
+
+	/**
+	 * 
+	 * @param num1
+	 * @param num2
+	 */
+	public void sumar(int num1, int num2) {
+
+	}
+}// end Estudiante Yo
